1c07442fd3ab9aa1c05c4990f26b9934301607f8
Victor Souza <38811882+viubis@users.noreply.github.com>
2022-02-16 13:31:14 -0300

    Create Questao2.cs

---
+++ Questao2.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace teste
+{
+    class Questao2
+    {
+        static void Main(string[] args)
+        {
+          double distancia,consumo,consumoMedio;
+          distancia=200;
+          consumo=10;
+
+          consumoMedio = distancia/consumo;
+
+          Console.WriteLine(consumoMedio);
+         }
+    }
+}
