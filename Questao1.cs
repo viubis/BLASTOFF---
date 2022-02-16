23549c616f090180e8dc1315e1875f1deea74e15
Victor Souza <38811882+viubis@users.noreply.github.com>
2022-02-16 13:29:22 -0300

    Create Questao1.cs

---
+++ Questao1.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace 
+{
+    class Questao1
+    {
+        static void Main(string[] args)
+        {
+          int i,j,k,x,y, total;
+          i=15;
+          j=30;
+          k=20;
+          x=25;
+          y=26;      
+
+          total=i+j+k+x+y;
+          total= total/5;     
+
+          Console.WriteLine(total);
+         }
+    }
+}
